52d4b2ecf5765403e227f691a6dc6b03b9415ec7
agnes <kagnesyun@gmail.com>
2020-08-17 01:41:04 +0900

    runcoding homepage ver01.

--- runcoding/urls.py
+++ runcoding/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls import url
+from . import views
+
 urlpatterns = [
-    path(r'^', include('elections.urls')),
+    url('', views.index),
     path('admin/', admin.site.urls),
 ]
