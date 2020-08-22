a6bbc8718767d4e137ed224714b1a1febdcd8213
andreabyfive <andreabyfive@gmail.com>
2020-08-22 21:31:16 +0900

    add DB

--- main/views.py
+++ main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from main.models import *
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'main/index_runcoding.html')
 
 # Create your views here.
+def UserRegister(request):
+    print("testing.....")
+    q = RegisterTB(regid=1, userid='ABB',name='OBY')
+    q.save()
+    return HttpResponse("OK")
--- runcoding/urls.py
+++ runcoding/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from main.views import *
 
 urlpatterns = [
+    path('register/', UserRegister),
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
