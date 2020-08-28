8aa69054800eda069cb8d689ae2fa1d3453879a5
agnes <kagnesyun@gmail.com>
2020-08-28 23:31:57 +0900

    print 삭제

--- main/query.py
+++ main/query.py
@@ -22,13 +22,12 @@ def select_prd(prd_code):
     return prd_info
 
 def update_order_prdCode(order_prd_info):
-        new_orderPrd = order_prd_info[0]
-        new_orderPrd.count = new_orderPrd.count + 1
-        new_orderPrd.save()
+    new_orderPrd = order_prd_info[0]
+    new_orderPrd.count = new_orderPrd.count + 1
+    new_orderPrd.save()
 
 def delete_login(user_id):
     login_info = select_login(user_id)
-
     if login_info.count() is not 0:
         new_login = login_info[0]
         new_login.dbstat = 'D-'
