2d9f8cc841c7acd600ec8feec1133b06a5c5689d
agnes <kagnesyun@gmail.com>
2020-08-28 23:29:48 +0900

    print 삭제

--- main/views.py
+++ main/views.py
@@ -68,8 +68,6 @@ def loungeView_page(request):
 def order_page(request):
     session = request.session.get('client_id')
     userid = request.session.get('user_id')  # 이 값으로 디비에서 정보찾고..
-    # print(session)
-
     if session is None:
         return render(request, 'login/login.html')
     else:
@@ -79,28 +77,13 @@ def order_page(request):
             "order_detail": order_info,
             "user_detail": user_info,
         }
-
         return render(request, 'payment/order.html', context)  # templete에 없으면 호출이 안됨. ajax
 
 
 def order(request):
     user_id = request.session.get('user_id')
-    session = request.session.get('client_id')
-
     login_info = select_login(user_id)
-    session_id = login_info[0].session_id
     messages = 0
-
-    # 에러남 수정해야됨
-    # check_session = bcrypt.checkpw(session.encode('utf-8'), session_id.encode('utf-8'))
-    # print(check_session)  # true/false
-    #
-    # if not check_session:
-    #     return render(request, 'login/login.html')
-    # else:
-
-    # order table에 저장하고 싶은데...resturn 메세지가 있어야 할듯.
-
     if login_info.count() is not 0:
         prd_code = request.POST['prd_code']
         order_prd_info = select_order_prdCode(user_id, prd_code)
@@ -112,14 +95,10 @@ def order(request):
             order = OrderTB(user_id=user_id, prd_code=prd_code, prd=prd_info[0])
             order.save()
             messages = 1  # 성공
-
     context = {
         "message": messages,
     }
     return render(request, 'class/class_view_arduino.html', context)
-
-    # Create your views here.
-
 
 # id로 검색해서 없으면 진행...있으면 에러리턴.
 def UserRegister(request):
@@ -129,19 +108,10 @@ def UserRegister(request):
     if regi_info.count() is 0:
         password = request.POST['regi_pass']
         password_encrypt = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-        # print(password_encrypt);
-
         q = RegisterTB(regi_id=regi_id, regi_name=request.POST['regi_name'],
                        regi_phone=request.POST['regi_phone'], regi_email=request.POST['regi_email'],
                        regi_add01=request.POST['regi_add01'], regi_add02=request.POST['regi_add02'],
                        regi_add03=request.POST['regi_add03'], regi_pass=password_encrypt.decode('utf-8'))
-
-        # from datetime import datetime
-        # from django.utils import timezone
-        # date_str = "2016-02-20 15:03:55"
-        # v = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
-
         q.save()
         return render(request, 'login/login.html')  # 로그인페이지호출
     else:
@@ -198,31 +168,21 @@ def logout(request):
     user_id = request.session.get('user_id')
     if user_id is not None:
         delete_login(user_id)
-
     request.session['client_id'] = ''
-
     return render(request, 'main/index_runcoding.html')
 
 
 def popup(request):
     regi_id = request.POST['popup_regiId']
     regi_info = select_register(regi_id)
-
     if regi_info.count() is not 0:
-        # print("Exist.....")
-        # request.session['IDresult'] = 'exist'
-
         context = {
             "popup_message": message_exist_id,  # 전역 변수로 변경 필요
         }
-
     else:
-        # print("DoesNotExist.....")
         context = {
             "popup_message": message_ok,
         }
-        # request.session['IDresult'] = 'ok'
-
     return render(request, 'popup/popup.html', context)
 
 
