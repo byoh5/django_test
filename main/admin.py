4d9ba21afc51ba9bf7e57d2d91b6e42d03f320a1
agnes <kagnesyun@gmail.com>
2020-09-13 17:12:19 +0900

    admin customizing

--- main/admin.py
+++ main/admin.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
 from main.models import *
-from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 from .models import RegisterTB
 # Register your models here.
 
 class RegisterAdmin(admin.ModelAdmin):
     list_display = ['regi_idx', 'regi_email', 'regi_name', 'regi_phone', 'stime', 'dbstat'] # 커스터마이징 코드
     list_display_links = ['regi_email', 'regi_name']
-    list_filter = ('dbstat',
-                   ('stime', DateRangeFilter),
-                   )
+
+    list_filter = ['stime', 'dbstat']
     search_fields = ['regi_email', 'regi_name']
 
 admin.site.register(RegisterTB, RegisterAdmin)
@@ -17,9 +15,7 @@ admin.site.register(RegisterTB, RegisterAdmin)
 class LoginAdmin(admin.ModelAdmin):
     list_display = ['user_id', 'login_time', 'dbstat'] # 커스터마이징 코드
     list_display_links = ['user_id']
-    list_filter = ('dbstat',
-                   ('login_time', DateRangeFilter),
-                   )
+    list_filter = ['login_time', 'dbstat']
     search_fields = ['user_id']
 
 admin.site.register(LoginTB,LoginAdmin)
@@ -27,8 +23,8 @@ admin.site.register(LoginTB,LoginAdmin)
 class PrdAdmin(admin.ModelAdmin):
     list_display = ['prd_code', 'title', 'period', 'price', 'keyword', 'dbstat'] # 커스터마이징 코드
     list_display_links = ['prd_code']
-    list_filter = ('dbstat',
-                   'prd_code',)
+    list_filter = ['prd_code', 'dbstat']
+
     search_fields = ['prd_code', 'title', 'keyword']
 
 admin.site.register(PrdTB, PrdAdmin)
@@ -54,10 +50,7 @@ class OrderAdmin(admin.ModelAdmin):
 
     get_name.short_description = 'prd code'  # Renames column head
 
-    list_filter = ('dbstat',
-                   ('order_time', DateRangeFilter),
-                   )
-
+    list_filter = ['order_time', 'dbstat']
     list_display_links = ['user_id']
     search_fields = ['user_id']
 
@@ -66,10 +59,7 @@ admin.site.register(OrderTB, OrderAdmin)
 class PayAdmin(admin.ModelAdmin):
     list_display = ['pay_num', 'get_name', 'prd_info', 'prd_total_price', 'pay_result', 'pay_time'] # 커스터마이징 코드
 
-    list_filter = (
-                   ('pay_time', DateRangeFilter),
-                   'pay_result',
-                   )
+    list_filter = ['pay_time', 'pay_result']
 
     def get_name(self, obj):
         return obj.pay_user.regi_email
@@ -84,12 +74,7 @@ admin.site.register(PayTB, PayAdmin)
 class MyClassListAdmin(admin.ModelAdmin):
     list_display = ['user_id', 'get_name', 'start_time', 'expire_time', 'dbstat'] # 커스터마이징 코드
 
-    list_filter = (
-                   ('start_time', DateRangeFilter),
-                   ('expire_time', DateRangeFilter),
-                   'dbstat',
-                   )
-
+    list_filter = ['start_time', 'expire_time','dbstat']
     def get_name(self, obj):
         return obj.prd.prd_code
 
@@ -104,9 +89,7 @@ admin.site.register(MyClassListTB, MyClassListAdmin)
 class ItemDowndataAdmin(admin.ModelAdmin):
     list_display = ['downdata_idx', 'downdata_name', 'prd_code', 'dbstat']  # 커스터마이징 코드
 
-    list_filter = (
-        'dbstat',
-    )
+    list_filter = ['dbstat']
 
     list_display_links = ['downdata_idx', 'downdata_name']
     search_fields = ['downdata_name', 'prd_code']
@@ -116,9 +99,7 @@ admin.site.register(ItemDowndataTB, ItemDowndataAdmin)
 class loungeListAdmin(admin.ModelAdmin):
     list_display = ['title', 'user', 'data_name', 'video_id', 'dbstat']  # 커스터마이징 코드
 
-    list_filter = (
-        'dbstat',
-    )
+    list_filter = ['dbstat']
 
     list_display_links = ['title', 'video_id']
     search_fields = ['video_id', 'title', 'user']
@@ -128,10 +109,7 @@ admin.site.register(loungeListTB, loungeListAdmin)
 class comunityAdmin(admin.ModelAdmin):
     list_display = ['category', 'title', 'dbstat']  # 커스터마이징 코드
 
-    list_filter = (
-        'dbstat',
-    )
-
+    list_filter = ['dbstat']
     list_display_links = ['category', 'title']
     search_fields = ['category', 'title']
 
@@ -140,10 +118,7 @@ admin.site.register(comunityTB, comunityAdmin)
 class categoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'dbstat']  # 커스터마이징 코드
 
-    list_filter = (
-        'dbstat',
-    )
-
+    list_filter = ['dbstat']
     list_display_links = ['name']
     search_fields = ['name']
 
