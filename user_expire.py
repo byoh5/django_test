3e55ba798f4debfbe0e346eac62c1e9b2625ea39
agnes <kagnesyun@gmail.com>
2020-09-14 19:56:34 +0900

    expire 로그

--- user_expire.py
+++ user_expire.py
@@ -14,16 +14,18 @@ def expire_user_class():
     month = timezone.now().month
     day = timezone.now().day-1
     expire_target = MyClassListTB.objects.filter(dbstat='A', expire_time__year=year, expire_time__month=month, expire_time__day=day)
-    print(expire_target.count())
-    print(expire_target[0].user_id)
 
-    for expire in expire_target:
-        update_myclass = expire
-        update_myclass.dbstat = 'D-' + str(year) + str(month) + str(day+1)
-        update_myclass.save()
+    if expire_target.count() != 0:
+        for expire in expire_target:
+            update_myclass = expire
+            update_myclass.dbstat = 'D-' + str(year) + str(month) + str(day+1)
+            update_myclass.save()
 
-    stat_info = statTB(expire_cnt=expire_target.count())
-    stat_info.save()
+        stat_info = statTB(expire_cnt=expire_target.count())
+        stat_info.save()
+        print("expire target is ", expire_target.count(), " (data : ", year, month, day, ")")
+    else:
+        print("expire target is 0 (data : ",year, month, day ,")")
 
 if __name__ == '__main__':
     expire_user_class()
