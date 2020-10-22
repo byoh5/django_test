03fde14067c09319addb52d7ff6f812b3d71146a
agnes <kagnesyun@gmail.com>
2020-10-22 20:54:41 +0900

    에러 처리

--- main/codeView/lecture.py
+++ main/codeView/lecture.py
@@ -14,19 +14,28 @@ def class_detail_page(request):
     prd_code = request.POST['detail_prd_code']
     items_info = select_class_detail(prd_code)
     html_file = ""
+    
+    if items_info.count() > 0:
+        if prd_code == '2020080013001':
+            html_file = "product/arduino_trashcan.html"
+        elif prd_code == '2020080023001':
+            html_file = "product/mblock_trashcan.html"
 
-    if prd_code == '2020080013001':
-        html_file = "product/arduino_trashcan.html"
-    elif prd_code == '2020080023001':
-        html_file = "product/mblock_trashcan.html"
+        context = {
+            "class_items_detail": items_info,
+            "prd_detail": items_info[0].prd,
+            "html_file": html_file,
+        }
 
-    context = {
-        "class_items_detail": items_info,
-        "prd_detail": items_info[0].prd,
-        "html_file": html_file,
-    }
+        return render(request, 'class/class_detail.html', context)
+    else:
+        user_id = request.session.get('user_id')
+        if user_id is not None:
+            delete_login(user_id)
+        request.session['client_id'] = ''
+        request.session['user_id'] = ''
 
-    return render(request, 'class/class_detail.html', context)
+        return render(request, 'main/index_runcoding.html')
 
 def class_detail_page_prd(request):
     prd_code = request.GET.get('prd_code')
