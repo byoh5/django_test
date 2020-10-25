f65bdf473c26bf3908c27ab8ba7d2a1242fa82c8
agnes <kagnesyun@gmail.com>
2020-10-25 21:12:09 +0900

    관찰카메라 테스트 버전

--- main/codeView/watch.py
+++ main/codeView/watch.py
@@ -4,8 +4,8 @@ HTTP_200_OK = 200
 HTTP_400_BAD_REQUEST = 400
 
 def watch_esp_cam(request):
-    data = request.GET.get('data', '0')
-    esp32_num = request.GET.get('esp32_num', '0')
+    data = request.POST.get('data', '0')
+    esp32_num = request.POST.get('esp32_num', '0')
 
     print(data, esp32_num)
 
