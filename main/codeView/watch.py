7a6777f464ee49f58b667b0d1548ba1067896c6e
agnes <kagnesyun@gmail.com>
2020-10-26 22:26:46 +0900

    관찰카메라 테스트 버전

--- main/codeView/watch.py
+++ main/codeView/watch.py
@@ -1,17 +1,15 @@
 from django.http import HttpResponse
-from django.middleware import csrf
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+
+import json
 
 HTTP_200_OK = 200
 HTTP_400_BAD_REQUEST = 400
 
 @method_decorator(csrf_exempt)
 def watch_esp_cam(request):
-    data = request.POST.get('data', '0')
-    esp32_num = request.POST.get('esp32_num', '0')
-
-    print(data, esp32_num)
-    print(csrf.get_token(request))
+    data = json.loads(request.body)
+    print(data)
 
     return HttpResponse(HTTP_200_OK)
