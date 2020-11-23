9d0960a62f8aaa995b1794cd7bc58082566c5fd5
agnes <kagnesyun@gmail.com>
2020-11-24 00:11:31 +0900

    네이버페이 연동 - xml 수정

--- main/codeView/prodcut_xml.py
+++ main/codeView/prodcut_xml.py
@@ -1,6 +1,7 @@
-from xml import etree
-from xml.etree.ElementTree import Element, SubElement, dump
+from xml.etree.ElementTree import Element, SubElement, dump, ElementTree
 
+from django.contrib.sites import requests
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from main.query import *
@@ -8,116 +9,105 @@ from main.query import *
 
 def product_xml_page(request):
     prd_code1 = request.GET.get('product[0][id]', "")
-
-    # if prd_code1 is not None:
-    #     xml_file = "all"
-
     prd_code2 = request.GET.get('product[1][id]', "")
-    xml_file = ""
 
-    if prd_code1 is not "" and prd_code2 is not "":
-        xml_file = "all"
-    elif prd_code1 is not None:
-        xml_file = prd_code1
-    elif prd_code2 is not None:
-        xml_file = prd_code2
+    return make_xml(prd_code1, prd_code2)
 
-    context = {
-        "xml_file": xml_file,
-    }
 
-    return render(request, 'product/xml.html', context)
+def indent(elem, level=0):
+    i = "\n" + level*" "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + " "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+            for elem in elem:
+                indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()): elem.tail = i
 
-def make_xml(order_info):
+
+
+def make_xml(prd_code1, prd_code2):
     # Create XML
     root = Element("products")
 
-    for order in order_info:
-        prd_code = order.prd.prd_code
-        print(prd_code)
-        prd_detail = select_prd(prd_code)
-        if prd_detail.count() > 0 :
+    prd_detail = select_prd(prd_code1)
+    if prd_detail.count() > 0:
 
-            x_merchantId = Element("merchantId")  # 상품 ID. 네이버페이에 가입 승인될 때 정해진다.
-            x_merchantId.text = ''
+        # Set product
+        x_product = Element("product")
 
-            x_certiKey = Element("certiKey")  # 인증키. 네이버페이에 가입 승인될 때 정해진다.
-            x_certiKey.text = ''
+        # Set id
+        x_id = SubElement(x_product, "id")
+        x_id.text = prd_detail[0].prd_code
 
+        # Set name
+        x_name = SubElement(x_product, "name")
+        x_name.text = prd_detail[0].title
 
-            # Set product
-            x_product = Element("product")
+        x_basePrice = SubElement(x_product, "basePrice")
+        x_basePrice.text = str(prd_detail[0].price)
 
-            # Set id
-            x_id = SubElement(x_product, "id")
-            x_id.text = prd_detail[0].prd_code
+        # Set infoUrl
+        x_infoUrl = SubElement(x_product, "infoUrl")
+        x_infoUrl.text = "http://runcoding.co.kr/detail_prd/?prd_code=" + prd_detail[0].prd_code
 
-            # Set name
-            x_name = SubElement(x_product, "name")
-            x_name.text = prd_detail[0].title
+        x_imageUrl = SubElement(x_product, "imageUrl")
+        x_imageUrl.text = "http://runcoding.co.kr/" + prd_detail[0].img
 
-            # Set basePrice
-            x_basePrice = SubElement(x_product, "basePrice")
-            x_basePrice.text = str(prd_detail[0].price)
+        x_status = SubElement(x_product, "status")
+        x_status.text = "ON_SALE"
 
-            # Set infoUrl
-            x_infoUrl = SubElement(x_product, "infoUrl")
-            x_infoUrl.text = "http://runcoding.co.kr/detail_prd/?prd_code=" + prd_detail[0].prd_code
+        x_option_support = SubElement(x_product, "optionSupport")
+        x_option_support.text = "true"
 
-            x_imageUrl = SubElement(x_product, "imageUrl")
-            x_imageUrl.text = "http://runcoding.co.kr/" + prd_detail[0].img
+        #options - 추가옵션구매
+        x_option = SubElement(x_product, "option")
 
-            # x_status = SubElement(x_product, "status")
-            # x_status.text = "ON_SALE"
+        x_quanItem = SubElement(x_option, "optionItem")
 
-            #supplement - 추가옵션구매
-            x_supplement = SubElement(x_product, "supplement")
+        x_option_type = SubElement(x_quanItem, "type")
+        x_option_type.text = 'SELECT'
 
-            x_groupId = SubElement(x_supplement, "groupId")
-            x_groupId.text = prd_detail[0].prd_code + "_1"
+        x_option_name = SubElement(x_quanItem, "name")
+        x_option_name.text = '추가부품'
 
-            x_supp_name = SubElement(x_supplement, "name")
-            x_supp_name.text = prd_detail[0].option1
+        x_option_value = SubElement(x_quanItem, "value")
 
-            if prd_detail[0].option1_price > 0 :
-                x_supp_price = SubElement(x_supplement, "price")
-                x_supp_price.text = str(prd_detail[0].option1_price)
+        x_option_value_id = SubElement(x_option_value, "id")
+        x_option_value_id.text = 'arduino'
 
-            x_supp_quantity = SubElement(x_supplement, "quantity")
-            x_supp_quantity.text = str(order.count)
+        x_option_value_txt = SubElement(x_option_value, "text")
+        x_option_value_txt.text = prd_detail[0].option1
 
-            #shipping
-            x_shippingPolicy = SubElement(x_product, "shippingPolicy")
+        #shipping
+        x_shippingPolicy = SubElement(x_product, "shippingPolicy")
 
-            x_sip_groupId = SubElement(x_shippingPolicy, "groupId")
-            x_sip_groupId.text = "sipping_" + order.pay_num
+        SubElement(x_shippingPolicy, "groupId")
 
-            x_sip_method = SubElement(x_shippingPolicy, "method")
-            x_sip_method.text = "DELIVERY"
+        x_sip_method = SubElement(x_shippingPolicy, "method")
+        x_sip_method.text = "DELIVERY"
 
-            x_sip_feeType = SubElement(x_shippingPolicy, "feeType")
-            x_sip_feeType.text = "CHARGE"
+        x_sip_feeType = SubElement(x_shippingPolicy, "feeType")
+        x_sip_feeType.text = "CHARGE"
 
-            x_sip_feePayType = SubElement(x_shippingPolicy, "feePayType")
-            x_sip_feePayType.text = "PREPAYED"
+        x_sip_feePayType = SubElement(x_shippingPolicy, "feePayType")
+        x_sip_feePayType.text = "PREPAYED"
 
-            x_sip_feePrice = SubElement(x_shippingPolicy, "feePrice")
-            x_sip_feePrice.text = str(order.delivery_price)
+        x_sip_feePrice = SubElement(x_shippingPolicy, "feePrice")
+        x_sip_feePrice.text = "3000"
 
-            #backUrl
-            x_backUrl  = SubElement(x_product, "backUrl")
-            x_backUrl.text = "http://runcoding.co.kr/order/"
+        root.append(x_product)
 
-            root.append(x_product)
+        indent(root)
+        dump(root)
+
+        ElementTree(root).write("runcoding.xml", encoding="utf-8", xml_declaration=True)
 
 
-
-    # Print
-    # x_output = etree.tostring(root, pretty_print=True, encoding='UTF-8', xml_declaration=True)
-    dump(root)
-    # ff = open('static/resource/data/xml/sample.xml', 'w', encoding='UTF-8')
-    # ff.write(x_output.decode('utf-8'))
-
-    return root
+    return HttpResponse(open('runcoding.xml', encoding='UTF-8').read(), content_type="application/xml")
 
 
